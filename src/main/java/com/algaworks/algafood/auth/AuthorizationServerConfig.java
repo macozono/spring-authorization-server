4dc97490ec6632e981f33198716780b0916bad71
Marcel Rodrigo Cozono <marcel.cozono@gmail.com>
2021-09-23 08:13:00 -0300

    Implementação de JWT com chave assimetrica.

--- src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
+++ src/main/java/com/algaworks/algafood/auth/AuthorizationServerConfig.java
@@ -1,7 +1,11 @@
 package com.algaworks.algafood.auth;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -10,6 +14,13 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.CompositeTokenGranter;
+import org.springframework.security.oauth2.provider.TokenGranter;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
@@ -22,7 +33,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	private AuthenticationManager authenticationManager;
 	
 	@Autowired
-	private UserDetailsService userDetailsServie;
+	private UserDetailsService userDetailsService;
+	
+	@Autowired
+	private JwtKeyStoreProperties jwtKeyStoreProperties;
+	
+//	@Autowired
+//	private RedisConnectionFactory redisConnectionFactory;
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -31,18 +48,22 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 			.secret(passwordEncoder.encode("web123"))
 			.authorizedGrantTypes("password", "refresh_token")
 			.scopes("write", "read")
-			.accessTokenValiditySeconds(60)
-			.refreshTokenValiditySeconds(120)
-		.and()
-			.withClient("algafood-mobile")
-			.secret(passwordEncoder.encode("mobile123"))
-			.authorizedGrantTypes("password", "refresh_token")
-			.scopes("write", "read")
-			.accessTokenValiditySeconds(60)
-			.refreshTokenValiditySeconds(120)
+			.accessTokenValiditySeconds(240)
+			.refreshTokenValiditySeconds(360)
 		.and()
 			.withClient("checktoken")
 			.secret(passwordEncoder.encode("check123"))
+		.and()
+			.withClient("analytics")
+			.secret(passwordEncoder.encode("lyt123"))
+			.authorizedGrantTypes("authorization_code")
+			.scopes("write", "read")
+			.redirectUris("http://aplicacao-cliente")
+		.and()
+			.withClient("webadmin")
+			.authorizedGrantTypes("implicit")
+			.scopes("write", "read")
+			.redirectUris("http://aplicacao-cliente")
 		.and()
 			.withClient("faturamento")
 			.secret(passwordEncoder.encode("fat123"))
@@ -53,13 +74,61 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		//security.checkTokenAccess("isAuthenticated()");
-		security.checkTokenAccess("permitAll()");
+		security.checkTokenAccess("permitAll()")
+			// Libera endpoint para geração da chave pública (endpoint /token_key)
+			.tokenKeyAccess("permitAll()")
+			.allowFormAuthenticationForClients();
 	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
 		endpoints.authenticationManager(authenticationManager)
-			.userDetailsService(userDetailsServie);
+			.userDetailsService(userDetailsService)
+			.tokenGranter(tokenGranter(endpoints))
+			.accessTokenConverter(jwtAccessTokenConverter())
+			// Chamar o método sempre depois do accessTokenConverter()
+			.approvalStore(approvalStore(endpoints.getTokenStore()));
+			//.tokenStore(redisTokenStore());
 			//.reuseRefreshTokens(Boolean.FALSE);
+	}
+	
+	private ApprovalStore approvalStore(TokenStore tokenStore) {
+		var approvalStore = new TokenApprovalStore();
+		approvalStore.setTokenStore(tokenStore);
+		
+		return approvalStore;
+	}
+	
+	@Bean
+	public JwtAccessTokenConverter jwtAccessTokenConverter() {
+		var jwt = new JwtAccessTokenConverter();
+		
+//		jwt.setSigningKey("dsaldjsaldjsalkjdsalj32u3u29dshady932hekahdndsakewoejwqdsajh");
+		
+		var jksResource = new ClassPathResource(jwtKeyStoreProperties.getPath());
+	    var keyStorePass = jwtKeyStoreProperties.getPassword();
+	    var keyPairAlias = jwtKeyStoreProperties.getKeypairAlias();
+		
+		var keyStoreKeyFactory = new KeyStoreKeyFactory(jksResource, keyStorePass.toCharArray());
+		var keyPair = keyStoreKeyFactory.getKeyPair(keyPairAlias);
+		
+		jwt.setKeyPair(keyPair);
+		
+		return jwt;
+	}
+	
+//	private TokenStore redisTokenStore() {
+//		return new RedisTokenStore(redisConnectionFactory);
+//	}
+	
+	private TokenGranter tokenGranter(AuthorizationServerEndpointsConfigurer endpoints) {
+		var pkceAuthorizationCodeTokenGranter = new PkceAuthorizationCodeTokenGranter(endpoints.getTokenServices(),
+				endpoints.getAuthorizationCodeServices(), endpoints.getClientDetailsService(),
+				endpoints.getOAuth2RequestFactory());
+		
+		var granters = Arrays.asList(
+				pkceAuthorizationCodeTokenGranter, endpoints.getTokenGranter());
+		
+		return new CompositeTokenGranter(granters);
 	}
 }
