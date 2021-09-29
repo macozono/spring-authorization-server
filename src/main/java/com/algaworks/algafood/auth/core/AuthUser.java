0b9e0267fd59a60f7049878c9ead967f569b0582
Marcel Rodrigo Cozono <marcel.cozono@gmail.com>
2021-09-29 07:52:49 -0300

    Modulo 23 - claims customizadas

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -1,7 +1,8 @@
 package com.algaworks.algafood.auth.core;
 
-import java.util.Collections;
+import java.util.Collection;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.algaworks.algafood.auth.domain.model.Usuario;
@@ -14,10 +15,12 @@ public class AuthUser extends User {
 	private static final long serialVersionUID = 1L;
 	
 	private String fullName;
+	private Long userId;
 	
-	public AuthUser(Usuario usuario) {
-		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+	public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
 		
+		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
 	}
 	
